4c4b6bcae08bc953c029b3161b28b94c5cda8d02
mryonom <start2093@gmail.com>
2022-06-25 18:55:39 +0900

    비밀정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mryonom:!!adam1587@yon.4vcjw.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
+const config = require('./config/key')
 
+const { User } = require('./models/User')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://mryonom:!!adam1587@yon.4vcjw.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDb Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.get('/', (req, res) => res.send('Hello World! GOOD'))
+
+app.post('/register', (req, res) => {
+    // 회원가입에 필요한 정보들을 client 에서 가져오면 DB에 저장한다.
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
